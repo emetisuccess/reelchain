d3223b4c8475d7bce1bebb907b15d8e7c46f5030
Emetisuccess <emetisuccess@gmail.com>
2024-07-23 13:06:51 +0100

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
@@ -21,38 +21,38 @@ const Navbar = () => {
     <div className="navbar rounded-3xl">
       <div onClick={handleClick} className={`${showNav ? "hamburger1" : "hamburger"}`}></div>
       <div className="logo">
-        <Link to="/">
+        <a href="/">
           <img src="logo1.png" alt="Logo" />
-        </Link>
+        </a>
         <nav className="menu">
           <ul className="inner-nav list-none">
             <li>
-              <Link to="/" className="text-2xl">
+              <a href="/" className="text-2xl">
                 Home
-              </Link>
+              </a>
             </li>
             <li>
-              <Link to="/mint" className="text-2xl">
+              <a href="/mint" className="text-2xl">
                 Mint
-              </Link>
+              </a>
             </li>
             <li>
-              <Link to="/waitlist" className="text-2xl">
+              <a href="/waitlist" className="text-2xl">
                 Waitlist
-              </Link>
+              </a>
             </li>
             <li>
-              <Link to="/doc" className="text-2xl">
+              <a href="/doc" className="text-2xl">
                 Doc
-              </Link>
+              </a>
             </li>
           </ul>
         </nav>
       </div>
       <div className="button">
-        <Link to="/contact" className="text-lg">
+        <a href="/contact" className="text-lg">
           Contact Us
-        </Link>
+        </a>
       </div>
     </div>
   );
