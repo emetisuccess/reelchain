28661d619f7ac5659e4443523e6ed20c3447ba89
Emetisuccess <emetisuccess@gmail.com>
2024-07-29 17:09:01 +0100

    third commit

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,8 +3,8 @@ const Container = () => {
     return (
         <div className="hero">
             <div className='container'>
-                <div className='text flex flex-column w-98'>
-                    <p className='text-white'>Stream, Earn, and Experience the Future of Entertainment with ReelChain.</p>
+                <div className='text flex flex-column w-3/5 md:w-3/5'>
+                    <p className='text-white font-bold'>Stream, Earn, and Experience the Future of Entertainment with ReelChain.</p>
                     <div className='button'><a href="/">Explore Now &#8599;</a></div>
                 </div>
             </div>
