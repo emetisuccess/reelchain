b21f07c63a2a2aaa47a97efefa97f9bd08569c42
Emetisuccess <emetisuccess@gmail.com>
2024-07-29 17:02:52 +0100

    third commit

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,9 +3,7 @@ const Container = () => {
     return (
         <div className="hero">
             <div className='container'>
-                <div className='text flex flex-column w-9/12'>
-                    <h3 className='text-white'>WEB3 Experience</h3>
-                    <h1 className='text-white'>ReelChain  AUTOMATION</h1>
+                <div className='text flex flex-column w-98'>
                     <p className='text-white'>Stream, Earn, and Experience the Future of Entertainment with ReelChain.</p>
                     <div className='button'><a href="/">Explore Now &#8599;</a></div>
                 </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,8 +7,7 @@ const Section = () => {
     <div className="section">
       <div className="inner-section">
         <div className="section-text py-16">
-          <h5>For traders</h5>
-          <h1>Prefered by traders with fully customisable API</h1>
+          <h5 className="font-bold text-3xl">VIewers</h5>
           <p>
             Experience the next evolution in entertainment! ReelChain combines blockchain technology with seamless streaming, allowing
             you to watch and earn $REEL tokens. Enjoy exclusive contents, secure your data, and participate in community governance.
@@ -22,24 +21,25 @@ const Section = () => {
       <div className="card-section">
         <div className="cards">
           <div className="card">
-            <img src={globe} alt="" />
-            <h1>Stream-to-Earn</h1>
+            <span className="text-3xl">&#x1F3A5;</span>
+            <h1 className="font-bold">Stream-to-Earn</h1>
             <p>
               Earn REEL tokens simply by watching your favorite movies, series, and documentaries. Your time and engagement are valuable,
               and we reward you for it!
             </p>
           </div>
           <div className="card">
-            <img src={two} alt="" />
-            <h1>Secure & Private</h1>
+            <span className="text-3xl" >&#x1F512;</span>
+
+            <h1 className="font-bold">Secure & Private</h1>
             <p>
               Your data is protected with advanced blockchain technology. Enjoy peace of mind knowing your information is safe and
               secure.
             </p>
           </div>
           <div className="card">
-            <img src={three} alt="" />
-            <h1>Community-Driven</h1>
+            <span className="text-3xl">&#x1F91D;</span>
+            <h1 className="font-bold">Community-Driven</h1>
             <p>
               Join a vibrant community of viewers who have a say in the platform&#39;s development and governance. Your voice matters
               here
